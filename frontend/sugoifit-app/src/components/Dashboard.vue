887df684fe98d880653c7fdc8850aeeda5c2f4b7
Krystall Messam <massem100@outlook.com>
2021-02-24 07:21:45 -0500

    ~ ~Started to style the side bar and dashboard but not done Added the add_transaction route for Cheyenne to add her stylings to. The sidebar.css is separated since its alot of code.

--- frontend/sugoifit-app/src/components/Dashboard.vue
+++ frontend/sugoifit-app/src/components/Dashboard.vue
@@ -1,5 +1,45 @@
 <template>
-<side-bar></side-bar>
+    <div class= "d-flex flex-row ">
+        <!-- Side Bar Component -->
+        <side-bar>Check</side-bar>
+       
+
+        <!-- Overview -->
+        <div class = "d-flex flex-column  w-100">
+            <div class="d-flex flex-row ml-auto">
+                <h6>Jane S.</h6>
+                <img src="" alt="">
+            </div>
+           <div class = "d-flex flex-row">
+            <div class = ""> 
+                <!-- Welcome + Msg -->
+                <h5 class =""> Welcome Back, <span> Jane</span> </h5>
+                <p class="">Lorem ipsum dolor sit amet consectetur adi</p>
+            </div>
+            <div class ="d-flex flex-row ml-auto p-3 m-4"> 
+                <div class ="d-flex flex-column ">
+                    <p class="time pr-2">7:00 PM</p>
+                    <p class="time_zone">EST</p>
+                </div>
+                <div class ="d-flex flex-column p-3">
+                    <p class="">12:00 AM</p>
+                    <p class="">LONDON</p>
+                </div>            
+            </div>
+        </div>
+            
+
+        
+
+            <!-- Quick Actions -->
+            <div class="d-flex flex-row p-2 text-center">
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span>Add Transaction</span></div>
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span> Place Order</span></div>
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span> Create Invoice</span></div>
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
--- frontend/sugoifit-app/src/components/SideBar.vue
+++ frontend/sugoifit-app/src/components/SideBar.vue
@@ -1,42 +1,36 @@
 <template>
-    <div class="page-content d-flex align-items-stretch">
-        <!-- Add a image of the user and their name to the top  -->
-        <!-- Side Navbar -->
-        <nav class="side-navbar ">           
-            <div class="sidebar-header d-flex align-items-center">
-                <div class="title">
-                    <div class = "d-flex flex-row">
-                        <img class = "mr-1" id="sidebar-image" src="">
-                        <div>
-                            <h1 class="h4">First name and last name</h1>
-                            <p> username</p>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Sidebar Navigation Menus-->
-            <span class="heading">Main</span>
+    <div>
+      <nav class ="side-navbar">
+          <!-- <img :src="navSVG" alt=""> -->
+          <img src="src/assets/align-left.png" alt="">
+          <h4 class = "nav_logo"> SugoiFit <span>Dashboard</span></h4>
+          
+          <div class="profile">
+              <h6> Jane S.</h6>
+              <h6> ID 1234567</h6>
+              <h6> Role: <span>Business Owner</span></h6>
+
+          </div>
+
+          <span class="heading">Main</span>
             <ul class="list-unstyled">
-                <li class="active"><a href="" >
-                    <img class= "home-icon" src = ""><i class="icon-home"> </i>Dashboard </a></li>
-                <!-- <li><a href="{{url_for('userprofile')}}"><img class="home-icon" src="/static/uploads/user.png"><i class="icon-grid"></i>My Profile </a></li>
-                <li><a href="#friendsdropdown" aria-expanded="false" data-toggle="collapse"> <img class="home-icon" src="/static/uploads/friend.png"><i class="fa fa-bar-chart"></i>Find Friends</a>
-                        <ul id="friendsdropdown" class="collapse list-unstyled ">
-                            <li><a href="{{url_for('friends')}}"> My Friends</a></li>
-                            <li><a href="{{url_for('searchfriends')}}">Search Friends</a></li>
-                        </ul>
-                </li>
-                 -->
+                <li class="active"><a href=""><img class= "home-icon" src = ""><i class="icon-home"></i>Dashboard </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>My Profile </a></li>
+            
                 </ul>
-        </nav>
-        </div>
+
+      </nav>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'SideBar',
+    data() {
+        return {
+            
+        }
+    }
 }
 </script>
 
