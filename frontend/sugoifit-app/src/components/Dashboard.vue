bb3d3b140d23011ac24376eeb110600b2afaa2ef
Krystall Messam <massem100@outlook.com>
2021-02-23 19:07:32 -0500

    Added Js Files

---
+++ frontend/sugoifit-app/src/components/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+<side-bar></side-bar>
+</template>
+
+<script>
+import SideBar from './SideBar.vue'
+export default {
+    components: { SideBar },
+    name: 'Dashboard',
+
+}
+</script>
