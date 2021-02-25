c390619cd18da6db709184b79f84edcea92a4eb5
Krystall Messam <massem100@outlook.com>
2021-02-25 03:35:37 -0500

    Revert "Added Dashboard Styling"
    This reverts commit 08aac906d4082b6c41366ae373429b3cd103f935.

--- frontend/sugoifit-app/src/components/SideBar.vue
+++ frontend/sugoifit-app/src/components/SideBar.vue
@@ -2,20 +2,25 @@
     <div>
       <nav class ="side-navbar">
           <!-- <img :src="navSVG" alt=""> -->
-          <img src="src/assets/align-left.png" alt="">
-          <h4 class = "nav_logo"> SugoiFit <span>Dashboard</span></h4>
-          
-          <div class="profile">
-              <h6> Jane S.</h6>
-              <h6> ID 1234567</h6>
-              <h6> Role: <span>Business Owner</span></h6>
-
+          <div class ="d-flex flex-row justify-content-center">
+            <img class = "nav_icon" src="static/uploads/align-left.png" alt="">
+            <h4 class = "nav_logo"> SugoiFit <span>Dashboard</span></h4>
+          </div>
+          <div class="profile ">
+                <h6 id = "profile_name"> Jane S.</h6>
+                <h6  id= "profile_ID"> ID 1234567</h6>
+                <img class ="" id = "profile-icon" :src="'/static/uploads/Profile_icon.png'" alt="">
+                <h6 id ="role"> Role: Business Owner</h6>     
           </div>
 
           <span class="heading">Main</span>
             <ul class="list-unstyled">
                 <li class="active"><a href=""><img class= "home-icon" src = ""><i class="icon-home"></i>Dashboard </a></li>
-                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>My Profile </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>Manage Products </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>Manage Sales </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>Manage Financial Statements </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i> View Reports </a></li>
+                <li><a href=""><img class="home-icon" src=""><i class="icon-grid"></i>Manage Roles </a></li>
             
                 </ul>
 
