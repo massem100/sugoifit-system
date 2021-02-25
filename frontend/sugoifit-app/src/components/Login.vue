a96bf9914336b9597dc4d7914fa39b4c969f592b
eversley1996 <everboy15@hotmail.com>
2021-02-25 00:31:28 -0400

    Login Component (Test)

---
+++ frontend/sugoifit-app/src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+         <div class = 'main-area'>
+                <form>
+                    <br>
+
+                    <br><br>
+                    Email:<br>
+                    <input type="text" name="email" id="email">
+                    <br><br>
+                    Password:<br>
+                    <input type="text" name="password" id="password">
+                </form>
+
+            </div>
+            <div id="result">
+
+            </div>
+
+            <div>
+                <button class="submit" id="submit" type="button">Submit</button>
+            </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data(){
+
+    }
+}
+</script>
+
+<style scoped>
+
+input[type=text], input[type=password] {
+  width: 60%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  border-radius: 10px;
+  background-color: Gainsboro;
+}
+
+button {
+  background-color: deepskyblue;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+  border-radius: 10px;
+  
+}
+</style>
