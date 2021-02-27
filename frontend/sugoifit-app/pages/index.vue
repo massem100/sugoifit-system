119e2af7f179698b1fffb4f43bf55a0dfbd034e3
massem100 <51300224+massem100@users.noreply.github.com>
2021-02-27 10:57:29 -0500

    Transferred Login,SignUp, Add Transaction Pages

--- frontend/sugoifit-app/pages/index.vue
+++ frontend/sugoifit-app/pages/index.vue
@@ -1,63 +1,75 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title bg-primary">sugoifit-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+    <div class= "d-flex flex-row ">
+        <!-- Side Bar Component -->
+        <side-bar>Check</side-bar>
+       
+
+        <!-- Overview -->
+        <div class = "d-flex flex-column w-100">
+            <div class ="d-flex flex-row align-items-center settings_bar ml-auto ">
+                <h6 class="d-flex ">Jane S.</h6>
+                <img class= "m-2" id = "top-user-icon" src="~assets/uploads/Profile_icon.svg" alt="">
+                <img class= "m-2" src="~assets/uploads/settings.svg" alt="">
+            </div>
+            <div class = "d-flex flex-row">
+                <div class = "welcome-heading">
+                    <h5 class =""> Welcome Back, <span> Jane</span> </h5>
+                    <p class="">Lorem ipsum dolor sit amet consectetur adi</p>
+                </div>
+
+                <div class ="time_box ml-auto"> 
+                    <p>7:00 PM</p>
+                    <p>EST</p>
+                    <p>12:00 AM</p>
+                    <p>LONDON</p>
+                </div> 
+            </div>
+            <!-- Quick Actions -->
+            <div class="quick_actions_div d-flex flex-row p-2 text-center">
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span>Add Transaction</span></div>
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span> Place Order</span></div>
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span> Create Invoice</span></div>
+                <div class = "d-flex flex-column quick_actions"><img src="" alt=""><i class="fas fa-plus"></i><span> Create Invoice</span></div>
+
+            </div>
+        </div>
+
+       
+        <!-- <div id="chartDiv" class ="" ></div> -->
+            
+        </div>
+    
 </template>
 
 <script>
-export default {}
+import SideBar from '/components/SideBar.vue'
+export default {
+    components: { SideBar },
+    name: 'Dashboard',
+    head(){
+        return{
+            title:"Dashboard",
+            meta: [
+                {
+                    hid:"description", 
+                    name: "description",
+                    content: "SugoiFit Business Management System Dashboard"
+                }
+            ]
+        };
+    }
+
+};
+
+
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped>
+#chartDiv{
+    width: 50%;
+    height: 30%;
+    position: absolute;
+    top: 300px;
+    right: 80px;
 }
 </style>
