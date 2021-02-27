b80460cde661aea07b7469b659105f52f08dc150
massem100 <51300224+massem100@users.noreply.github.com>
2021-02-27 17:38:54 -0500

    Added CORS to backend and website.css link

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 
 
@@ -7,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
 
-
+cors = CORS(app)
 
 app.config.from_object(__name__)
 from app import views
--- frontend/sugoifit-app/nuxt.config.js
+++ frontend/sugoifit-app/nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'static/scss/sidebar.css',
-    'static/scss/style.css'
+    'static/scss/style.css',
+    'static/scss/website.css'
+
+
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
