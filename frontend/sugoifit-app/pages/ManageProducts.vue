26958b840944b336b88290b91ed7426be973183e
massem100 <51300224+massem100@users.noreply.github.com>
2021-03-27 19:20:42 -0500

    Updated Main Readme.md

--- frontend/sugoifit-app/pages/ManageProducts.vue
+++ frontend/sugoifit-app/pages/ManageProducts.vue
@@ -5,67 +5,67 @@
         <div class="products-page ">
             <div>
             <b-tabs content-class="mt-3" fill>
-                <b-tab title="First" active>
+                <b-tab title="Add New Product" active>
                     <div class="sys-head w-100  ">
-                 <div class="mr-auto head-title">
-                    <h5> Manage Products</h5>
-                    <p> Mange your products here </p>
-                </div>
-               
-            </div>
-           <div class ="product-card">
-                <h5> Product Overview</h5>ur
-                <p class="prod-p">Lorem ipsum dolor sit amet,  iste at facilis eos architecto 
-                    alias perferendis veritatis dolor ut cupiditate repellat reiciendis maxime,
-                     ipsam voluptatibus blanditiis quibusdam.  Lorem ipsum dolor, sititaque ab 
-                     obcaecati modi saepe minus necessitatibus?</p>
-            </div>
-            <div class = "products-main">
-               
+                            <div class="mr-auto head-title">
+                                <h5> Manage Products</h5>
+                                <p> Mange your products here </p>
+                            </div>
+                        
+                        </div>
+                    <div class ="product-card">
+                            <h5> Product Overview</h5>ur
+                            <p class="prod-p">Lorem ipsum dolor sit amet,  iste at facilis eos architecto 
+                                alias perferendis veritatis dolor ut cupiditate repellat reiciendis maxime,
+                                ipsam voluptatibus blanditiis quibusdam.  Lorem ipsum dolor, sititaque ab 
+                                obcaecati modi saepe minus necessitatibus?</p>
+                        </div>
+                        <div class = "products-main">
+                        
 
-                <div class="formData"> 
-                    <div>
-                        <b-label for="searchBox">Search</b-label>
-                        <b-form-input type="text" name="SearchBox" class="searchBox"></b-form-input>
-                        <button class="buttons" type="button">Search</button>
-                    
-                    </div>
+                            <div class="formData"> 
+                                <div>
+                                    <b-label for="searchBox">Search</b-label>
+                                    <b-form-input type="text" name="SearchBox" class="searchBox"></b-form-input>
+                                    <button class="buttons" type="button">Search</button>
+                                
+                                </div>
 
-                    <div class="prod-item">
-                        <b-label for="prodName"> Product Name </b-label>
-                        <b-form-input type="text" name="ProductName" class="prodName"></b-form-input>
-                    </div>
-                    <div class="prod-item">
-                        <b-label for="qtyinstock"> Quantity In Stock </b-label>
-                        <b-form-input type="number" name="QtyInStock" class="qtyinstock"></b-form-input>
-                    </div>
-                    <div class="prod-item">
-                        <b-label for="unitPrice"> Unit Price </b-label>
-                        <b-form-input type="number" name="UnitPrice" class="unitPrice"></b-form-input>
-                    </div>
-                    <div class="prod-item">
-                        <b-label for="threshold"> Threshold </b-label>
-                        <b-form-input type="number" name="threshold" class="threshold" min="0" step="1"></b-form-input>
-                    </div>
-                    <div class="prod-item">
-                        <b-label for="tax"> Tax </b-label>
-                        <b-form-input type="number" name="tax" class="tax" min="0" step="0.01"></b-form-input>
-                    </div>
-                    <div class="prod-item"> 
-                        <textarea rows= "3" cols="40" name = "description">
-                            Enter descripton here...
-                        </textarea>
-                    </div>
-                    
-                
-                </div>
-                <div class="crudBtn"> 
-                        <button class="buttons" type="button">Update</button>
-                        <button class="buttons" type="button">Delete</button>
-                        <button class="buttons" type="button">Cancel</button>
-                </div>
+                                <div class="prod-item">
+                                    <b-label for="prodName"> Product Name </b-label>
+                                    <b-form-input type="text" name="ProductName" class="prodName"></b-form-input>
+                                </div>
+                                <div class="prod-item">
+                                    <b-label for="qtyinstock"> Quantity In Stock </b-label>
+                                    <b-form-input type="number" name="QtyInStock" class="qtyinstock"></b-form-input>
+                                </div>
+                                <div class="prod-item">
+                                    <b-label for="unitPrice"> Unit Price </b-label>
+                                    <b-form-input type="number" name="UnitPrice" class="unitPrice"></b-form-input>
+                                </div>
+                                <div class="prod-item">
+                                    <b-label for="threshold"> Threshold </b-label>
+                                    <b-form-input type="number" name="threshold" class="threshold" min="0" step="1"></b-form-input>
+                                </div>
+                                <div class="prod-item">
+                                    <b-label for="tax"> Tax </b-label>
+                                    <b-form-input type="number" name="tax" class="tax" min="0" step="0.01"></b-form-input>
+                                </div>
+                                <div class="prod-item"> 
+                                    <textarea rows= "3" cols="40" name = "description">
+                                        Enter descripton here...
+                                    </textarea>
+                                </div>
+                                
+                            
+                            </div>
+                            <div class="crudBtn"> 
+                                    <button class="buttons" type="button">Update</button>
+                                    <button class="buttons" type="button">Delete</button>
+                                    <button class="buttons" type="button">Cancel</button>
+                            </div>
 
-            </div>
+                        </div>
                 </b-tab>
                 <b-tab title="Second"><p>I'm the second tab</p></b-tab>
                 <b-tab title="Very, very long title">
